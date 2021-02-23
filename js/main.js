ac0b3d61acaf05c16bf9114d23ec51c4516c7c73
Azizbek <developer20010331@gmail.com>
2021-02-23 15:05:23 +0500

    one

--- js/main.js
+++ js/main.js
@@ -28,4 +28,3 @@ let letter = '';
 }());
 
 
-// vertical carusel
--- js/navbarr.js
+++ js/navbarr.js
@@ -4,33 +4,3 @@ window.addEventListener('scroll', event =>{
     (window.scrollY >= 44) ? nav.classList.add('scroll') : nav.classList.remove('scroll');
 });
 
-
-// navigation scroll active comment
-// window.addEventListener('scroll', event =>{
-//     let navigatActive = document.querySelectorAll('header .container .row .navbar .collapse ul li a');
-//     let fromTop = window.scrollY;
-
-//     navigatActive.forEach(link =>{
-//      let sectionn = document.querySelector(link.hash);
-
-//      if(
-//          sectionn.offsetTop <= fromTop && 
-//          sectionn.offsetTop + sectionn.offsetHeight > fromTop
-//      )
-//      {
-//          link.classList.add('active');
-//      } else {
-//          link.classList.remove('active');
-//      }
-//     });
-// })
-
-// const currentLocation = location.href;
-// const menuItem = document.getElementsByName('vavv');
-// const menuLength = menuItem.length;
-// for (let i=0; i<menuLength; i++) {
-//     if(menuItem[i].href === currentLocation){
-//         menuItem[i].className = 'active';
-//     }
-// } 
-
